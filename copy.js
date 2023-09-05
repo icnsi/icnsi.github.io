1efac22e0fd4fff9e86918b072b6558d01386a26
icnsi <114055956+icnsi@users.noreply.github.com>
2023-09-05 18:07:27 +0700

    Update copy.js

--- copy.js
+++ copy.js
@@ -1,9 +1,25 @@
-function myFunction() {
-  
-  var copyText = document.getElementById("myInput");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
-  navigator.clipboard.writeText(copyText.value);
-  alert("Tautan telah disalin: " + copyText.value);
-  
-}
+const textToCopy = "https://shiroharu.eu.org";
+const buttons = document.querySelectorAll('.copy-button');
+
+  buttons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    const textarea = document.createElement('textarea');
+    textarea.value = textToCopy;
+
+    document.body.appendChild(textarea);
+
+    textarea.select();
+
+    document.execCommand('copy');
+
+    document.body.removeChild(textarea);
+
+    button.innerText = 'Tautan telah disalin!';
+    button.disabled = true;
+
+    setTimeout(function() {
+      button.innerText = 'Salin Tautan';
+      button.disabled = false;
+    }, 2000);
+  });
+});
